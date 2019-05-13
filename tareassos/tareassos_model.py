db2178881cc32d233647fca1fc898a6b548e75c7
Sebas O.S <sebasdeejay3@gmail.com>
2019-05-13 23:27:45 +0200

    model.py OK
    acabada tareassos model py.  Con titulo, descripcion, terminada y activa

---
+++ tareassos/tareassos_model.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+ 
+class tareassos(models.Model):
+	_name = "tareassos.tareassos"
+	titulo = fields.Char(string='Titulo Tarea', required=True)
+	descripcion = fields.Text('Descripción', required=True,)
+	terminada = fields.Boolean('Terminada', default=False, help='')
+	activa = fields.Boolean('¿Tarea activa?', default=True) 
