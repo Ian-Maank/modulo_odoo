f2d721e3690b9976ead0504f3f6f1dd5b374c347
Sebas O.S <sebasdeejay3@gmail.com>
2019-05-13 18:37:24 +0200

    manifest ok
    fichero manifest completado

---
+++ tareassos/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'tareassos',
+	'summary' : 'Módulo realizado por SOS, SGE 18-19',
+    'version' : '1.0',
+	'licence' : 'GPL-3',
+	'website' : 'https://www.orantes.com',
+	'category' : 'Tools',
+    'author' : 'Sebastián Orantes Solera',
+    'description' : 'Con este módulo administraremos tareas',
+    'depends' : ['base',],
+    'data' : ['tareassos_view.xml',],
+	'demo':[],
+    'installable' : True,
+    'auto_install' : False,    
+}
