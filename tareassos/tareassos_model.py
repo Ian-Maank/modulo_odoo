a2933512970e22107d2f0c765484a0aca1475fae
Sebas O.S <sebasdeejay3@gmail.com>
2019-05-13 23:41:00 +0200

    añadido Photo

--- tareassos/tareassos_model.py
+++ tareassos/tareassos_model.py
@@ -6,5 +6,6 @@ class tareassos(models.Model):
 	_name = "tareassos.tareassos"
 	titulo = fields.Char(string='Titulo Tarea', required=True)
 	descripcion = fields.Text('Descripción', required=True,)
+	photo = fields.Binary(string='Photo')
 	terminada = fields.Boolean('Terminada', default=False, help='')
 	activa = fields.Boolean('¿Tarea activa?', default=True) 
